5d08ba544a0119c6dc79f859874a358ee0fe8acf
Sam Rae <samuelrae@gmail.com>
2015-12-25 22:10:31 +0000

    filtered out tags with beef and barbecue as no icons for those. First tried for ages to filter by all the tags that there are icons for

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,17 @@
  */
 angular.module('jstestApp')
   .controller('MainCtrl', ['$scope', 'MenuService', function ($scope, MenuService) {
-	$scope.menu = {};
-    MenuService.get('/data/menu.json').success(function(data) {
-	  $scope.menu = data;
-	});
+  	$scope.menu = {};
+      MenuService.get('/data/menu.json').success(function(data) {
+  	  $scope.menu = data;
+  	});
 
+    $scope.filterBy = 'charcoal cheese chicken grilled high-protein lamb pasta peanut pork seafood snack spicy starter sweet vegetarian'.split(' ');
+
+    $scope.filterTags = function (tags) {
+      return tags.filter(function(tag) {
+        return $scope.filterBy.indexOf(tag) !== -1;
+      });
+    };
   }
 ]);
